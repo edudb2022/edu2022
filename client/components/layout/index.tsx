4c02552cc57f553b6002791140bf4c223dc85f92
Pjaijai <paul6a24@hotmail.com>
2022-04-27 14:54:59 +0800

    add layout

---
+++ client/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../footer";
+
+const Layout: React.FunctionComponent = ({ children }) => {
+  return (
+    <>
+      {children} <Footer />
+    </>
+  );
+};
+
+export default index;
