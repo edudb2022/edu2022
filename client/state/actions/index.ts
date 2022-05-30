a988b0b8ca7bceb14609cd75e834119d0543f33a
Pjaijai <paul6a24@hotmail.com>
2022-05-30 19:20:42 +0800

    delete root action

--- client/state/actions/index.ts
+++
@@ -1,6 +0,0 @@
-// import { ISystemActionTypes } from "../system/types";
-// import { IUserActionTypes } from "../user/types";
-
-// // export type IRootActionTypes = ISystemActionTypes | IUserActionTypes;
-
-// const IRootActionTypes = { ...ISystemActionTypes, ...IUserActionTypes };
