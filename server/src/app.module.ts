b2d24ad7e397375a86333ab9c6ce0fcda32c858a
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-05 22:00:01 +0800

    database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { SchoolsModule } from "./schools/schools.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), SchoolsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get("DATABASE_HOST"),
+        port: configService.get("DATABASE_PORT"),
+        username: configService.get("DATABASE_USERNAME"),
+        password: configService.get("DATABASE_PASSWORD"),
+        database: configService.get("DATABASE_NAME"),
+      }),
+    }),
+    SchoolsModule,
+  ],
   controllers: [],
   providers: [],
 })
