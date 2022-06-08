9f350c39f38cd24e2fe40816e2553ff2e1c3d99c
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-08 23:57:15 +0800

    school-review entity

--- server/src/school-reviews/entities/school-review.entity.ts
+++ server/src/school-reviews/entities/school-review.entity.ts
@@ -1,11 +1,34 @@
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { School } from "../../schools/entities/school.entity";
+import { User } from "../../users/entities/user.entity";
 
 @Entity({ name: "SchoolReview" })
 export class SchoolReview {
   @PrimaryGeneratedColumn()
   id: number;
 
+  // TODO - Make school and user unique fields
   @ManyToOne(() => School, school => school.reviews)
   school: School;
+
+  @ManyToOne(() => User, user => user.schoolReviews)
+  user: User;
+
+  @Column("varchar", { length: 255 })
+  title: string;
+
+  @Column("timestamp", { default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date;
+
+  @Column("year")
+  admissionYear: number;
+
+  @Column("int")
+  likes: number;
+
+  @Column("int")
+  dislikes: number;
+
+  @Column("tinyint")
+  anonymous: boolean;
 }
