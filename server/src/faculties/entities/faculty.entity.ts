a108cabc06a63738a8fa0d69a9ee6720150cf80d
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-08 23:02:18 +0800

    program entity

--- server/src/faculties/entities/faculty.entity.ts
+++ server/src/faculties/entities/faculty.entity.ts
@@ -1,12 +1,23 @@
-import { Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Program } from "../../programs/entities/program.entity";
 import { School } from "../../schools/entities/school.entity";
 
+@Entity()
 export class Faculty {
   @PrimaryGeneratedColumn()
   id: number;
 
   @ManyToOne(() => School, school => school.faculties)
   school: School;
+
+  @OneToMany(() => Program, program => program.faculty)
+  programs: Program[];
 
   @Column({
     type: "varchar",
--- server/src/programs/entities/program.entity.ts
+++ server/src/programs/entities/program.entity.ts
@@ -1,1 +1,17 @@
-export class Program {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Faculty } from "../../faculties/entities/faculty.entity";
+
+@Entity()
+export class Program {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Faculty, faculty => faculty.programs)
+  faculty: Faculty;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+  })
+  name: string;
+}
