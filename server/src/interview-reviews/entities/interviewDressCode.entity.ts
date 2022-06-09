2f27072e632e38c3e52c40a439524dd17fc9e0cf
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-09 22:40:51 +0800

    interview review dress code enum

--- server/src/interview-reviews/entities/interviewDressCode.entity.ts
+++ server/src/interview-reviews/entities/interviewDressCode.entity.ts
@@ -1,5 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum InterviewDressCodeEnum {
+  CASUAL = 1,
+  SMART_CASUAL = 2,
+  FORMAL = 3,
+  FREE = 4,
+}
+
 @Entity()
 export class InterviewDressCode {
   @PrimaryGeneratedColumn()
