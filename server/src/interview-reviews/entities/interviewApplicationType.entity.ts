13e7c7e462791a96c271cf60286b33c651ba4118
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-09 22:42:08 +0800

    interview review application type enum

--- server/src/interview-reviews/entities/interviewApplicationType.entity.ts
+++ server/src/interview-reviews/entities/interviewApplicationType.entity.ts
@@ -1,5 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum InterviewApplicationTypeEnum {
+  JUPAS = 1,
+  NON_JUPAS = 2,
+}
+
 @Entity()
 export class InterviewApplicationType {
   @PrimaryGeneratedColumn()
