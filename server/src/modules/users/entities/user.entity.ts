f8326296adf034dd4ebc7fcdd49e5755f37a6f53
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-11 21:47:03 +0800

    update user one to many columns

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -1,4 +1,6 @@
 import { AdmissionOfferReview } from "@modules/admission-offer-reviews/entities";
+import { GradJobReview } from "@modules/grad-job-reviews/entities/grad-job-review.entity";
+import { InternshipReview } from "@modules/internship-reviews/entities/internship-review.entity";
 import { InterviewReview } from "@modules/interview-reviews/entities";
 import { ProgramReview } from "@modules/program-reviews/entities";
 import { SchoolReview } from "@modules/school-reviews/entities";
@@ -26,4 +28,8 @@ export class User {
     admissionOfferReview => admissionOfferReview.user
   )
   admissionOfferReviews: AdmissionOfferReview[];
+  @OneToMany(() => GradJobReview, gradJobReview => gradJobReview.user)
+  gradJobReviews: GradJobReview[];
+  @OneToMany(() => InternshipReview, internshipReview => internshipReview.user)
+  internshipReviews: InternshipReview[];
 }
