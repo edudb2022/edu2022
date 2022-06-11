024cd9359ded70c3ff51e3fe5812491269cba973
Andrew781123 <leechihoandrew1123@gmail.com>
2022-06-11 22:24:06 +0800

    job post source enum

--- server/src/common/entities/jobs/job-post-source.entity.ts
+++ server/src/common/entities/jobs/job-post-source.entity.ts
@@ -1,5 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum JobPostSourceEnum {
+  JIJIS = 1,
+  JOBS_DB,
+  INDEED,
+  OTHER,
+}
+
 @Entity()
 export class JobPostSource {
   @PrimaryGeneratedColumn()
