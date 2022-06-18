4f1016fe201286dc8339f5e21d95c56052d56b23
Pjaijai <paul6a24@hotmail.com>
2022-06-18 18:24:15 +0800

    create base teg

--- client/components/common/tags/index.tsx
+++ client/components/common/tags/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from "react"
 
-const Base = () => {
-  return <div>Base</div>;
-};
+interface IBaseTagProps {
+  title: string
+  secondary?: boolean
+  // color: string
+  className?: string
+}
 
-export default Base;
+//     <div className={`${secondary ?  `border-2 boder-${color} text-${color}` : `boder-${color} bg-${color}` } ${className}`}>{title}</div>
+// }
+
+const BaseTag: React.FunctionComponent<IBaseTagProps> = ({
+  title,
+  secondary,
+  // color,
+  className
+}) => {
+  return <div className={`rounded-2xl px-7  ${className}`}>{title}</div>
+}
+
+export default BaseTag
