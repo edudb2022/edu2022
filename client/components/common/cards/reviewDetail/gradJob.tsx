0296a1b6353c458eb4b1316940302c83966d4ee6
Pjaijai <paul6a24@hotmail.com>
2022-06-18 21:00:25 +0800

    .

--- client/components/common/cards/reviewDetail/gradJob.tsx
+++ client/components/common/cards/reviewDetail/gradJob.tsx
@@ -1,25 +1,27 @@
 import React from "react"
 import BaseReviewDetailCard, { IBaseReviewDetailCardProps } from "."
 import { ID } from "../../../../types/common"
-import ReviewBarRankItem from "./items/rank"
+import ReviewNumberItem from "./items/number"
 import ReviewTextItem from "./items/text"
 
 interface IGradJobReviewDetailCard extends IBaseReviewDetailCardProps {
   industry: string
   gradDate: string
   id: ID
+  offerDate: string
+  salary: number
 }
 
 const GradJobReviewDetailCard: React.FunctionComponent<
   IGradJobReviewDetailCard
-> = ({ ...props }) => {
+> = ({ industry, gradDate, offerDate, salary, ...props }) => {
   return (
     <BaseReviewDetailCard {...props}>
       <div className="flex flex-row justify-around">
-        <ReviewTextItem detail="123" title="行業" />
-        <ReviewTextItem detail="02/23" title="畢業日期" />
-        <ReviewTextItem detail="02/23" title="offer日期" />
-        <ReviewTextItem detail="1123120" title="年薪" />
+        <ReviewTextItem detail={industry} title="行業" />
+        <ReviewTextItem detail={gradDate} title="畢業日期" />
+        <ReviewTextItem detail={offerDate} title="offer日期" />
+        <ReviewNumberItem detail={salary} title="年薪" />
       </div>
     </BaseReviewDetailCard>
   )
--- client/pages/review/tertiary/admission/[id].tsx
+++ client/pages/review/tertiary/admission/[id].tsx
@@ -34,7 +34,6 @@ const AdmissionOfferReviewPage: NextPage = () => {
           nonJupasBestGpaValue={3.3}
           postDate="20/23"
           score={23}
-          title="123"
           label="23"
           id="23"
           isStudent={true}
