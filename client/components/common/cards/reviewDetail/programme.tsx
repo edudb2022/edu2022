73acb9a57283ec0765e5b52d03a3ffcccf504cae
paulwonggogo <paul.wong@gogox.com>
2022-06-21 11:17:10 +0800

    update programme review detail card

--- client/components/common/cards/reviewDetail/programme.tsx
+++ client/components/common/cards/reviewDetail/programme.tsx
@@ -2,6 +2,8 @@ import { Rating } from "@mui/material"
 import React from "react"
 import BaseReviewDetailCard, { IBaseReviewDetailCardProps } from "."
 import { ID } from "../../../../types/common"
+import RantingTag from "../../tags/ranking"
+import RankingTag from "../../tags/ranking"
 
 interface IProgrammeReviewDetailCardProps extends IBaseReviewDetailCardProps {
   value: number
@@ -15,8 +17,13 @@ const ProgrammeReviewDetailCard: React.FunctionComponent<
   return (
     <BaseReviewDetailCard {...props}>
       <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
-        <Rating name="read-only" value={value} readOnly className="text-5xl" />
-        <h1>{label}</h1>
+        {/* <Rating name="read-only" value={value} readOnly className="text-5xl" /> */}
+        <RantingTag
+          ranting={1}
+          title={`1.4 ${label}`}
+          className=" md:text-2xl text-lg md:p-4 p-2 font-black flex justify-center items-center"
+        />
+        {/* <h1>{label}</h1> */}
       </div>
     </BaseReviewDetailCard>
   )
