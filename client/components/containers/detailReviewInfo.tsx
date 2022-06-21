fefb980f58295c53b99e91880b8cf4540ee33235
Pjaijai <paul6a24@hotmail.com>
2022-06-21 21:43:57 +0800

    add review detail info container

--- client/components/containers/detailReviewInfo.tsx
+++ client/components/containers/detailReviewInfo.tsx
@@ -1,11 +1,66 @@
 import React from "react"
 
-interface IDetailReviewInfoContainerProps {}
+interface IDetailReviewInfoContainerProps {
+  className?: string
+  postDate: string
+  username: string | null
+  contact: string
+  admissionYear?: string
+  academicStatus?: string
+  interviewDate: string
+  DressCode?: string
+  offerDate?: string
+  currentSchool?: string
+  currentProgramme?: string
+  industry?: string
+  companyName?: string
+  jobSource?: string
+}
 
 const DetailReviewInfoContainer: React.FunctionComponent<
   IDetailReviewInfoContainerProps
-> = () => {
-  return <>123</>
+> = ({
+  className,
+  postDate,
+  username,
+  contact,
+  admissionYear,
+  interviewDate,
+  academicStatus,
+  DressCode,
+  offerDate,
+  currentSchool,
+  currentProgramme,
+  industry,
+  companyName,
+  jobSource
+}) => {
+  return (
+    <div className={`${className}`}>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-around flex-wrap">
+          {admissionYear && <p> {`admissionYear : ${admissionYear}`}</p>}
+          {interviewDate && <p> {`interviewDate : ${interviewDate}`}</p>}
+          {academicStatus && <p> {`academicStatus : ${academicStatus}`}</p>}
+          {currentSchool && <p> {`currentSchool : ${currentSchool}`}</p>}
+          {currentProgramme && (
+            <p> {`currentProgramme : ${currentProgramme}`}</p>
+          )}
+          {industry && <p> {`industry  : ${industry}`}</p>}
+          {companyName && <p> {`companyName  : ${companyName}`}</p>}
+          {jobSource && <p> {`jobSource  : ${jobSource}`}</p>}
+          {offerDate && <p> {`offerDate : ${offerDate}`}</p>}
+          {DressCode && <p> {`DressCode : ${DressCode}`}</p>}
+        </div>
+
+        <div className="flex flex-row">
+          <p> {`username  : ${username ? username : "null"}`}</p>
+          <p> {`Post Date : ${postDate}`}</p>
+          <p> {`contact : ${contact ? contact : "null"}`}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DetailReviewInfoContainer
