9ad909f14ef2362b8c91c776d0d408a160a2ce4e
paulwonggogo <paul.wong@gogox.com>
2022-06-21 11:39:59 +0800

    add review detail page

---
+++ client/pages/reviewDetail/tertiary/school/[id].tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+import React from "react"
+
+const SchoolReviewDetailPage: NextPage = () => {
+  return <div>school12</div>
+}
+
+export default SchoolReviewDetailPage
