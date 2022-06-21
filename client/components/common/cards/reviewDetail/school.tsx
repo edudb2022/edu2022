f9a99a7cd4bb3f6d0e9ce1ba5f4705877b4e6413
paulwonggogo <paul.wong@gogox.com>
2022-06-21 11:17:45 +0800

    updating school review detail card

--- client/components/common/cards/reviewDetail/school.tsx
+++ client/components/common/cards/reviewDetail/school.tsx
@@ -2,6 +2,7 @@ import { Rating } from "@mui/material"
 import React from "react"
 import BaseReviewDetailCard, { IBaseReviewDetailCardProps } from "."
 import { ID } from "../../../../types/common"
+import RantingTag from "../../tags/ranking"
 
 interface ISchoolReviewDetailCardProps extends IBaseReviewDetailCardProps {
   value: number
@@ -15,8 +16,13 @@ const SchoolReviewDetailCard: React.FunctionComponent<
   return (
     <BaseReviewDetailCard {...props}>
       <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
-        <Rating name="read-only" value={value} readOnly className="text-5xl" />
-        <h1>{label}</h1>
+        {/* <Rating name="read-only" value={value} readOnly className="text-5xl" />
+        <h1>{label}</h1> */}
+        <RantingTag
+          ranting={1}
+          title={`1.4 ${label}`}
+          className=" md:text-2xl text-lg md:p-4 p-2 font-black flex justify-center items-center"
+        />
       </div>
     </BaseReviewDetailCard>
   )
