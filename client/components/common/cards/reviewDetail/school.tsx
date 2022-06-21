49d0cee9d743a790f9a5e119ed3c71cd7d383b7b
paulwonggogo <paul.wong@gogox.com>
2022-06-21 11:39:51 +0800

    add link to review detail card

--- client/components/common/cards/reviewDetail/school.tsx
+++ client/components/common/cards/reviewDetail/school.tsx
@@ -1,4 +1,5 @@
 import { Rating } from "@mui/material"
+import Link from "next/link"
 import React from "react"
 import BaseReviewDetailCard, { IBaseReviewDetailCardProps } from "."
 import { ID } from "../../../../types/common"
@@ -12,18 +13,22 @@ interface ISchoolReviewDetailCardProps extends IBaseReviewDetailCardProps {
 
 const SchoolReviewDetailCard: React.FunctionComponent<
   ISchoolReviewDetailCardProps
-> = ({ value, label, ...props }) => {
+> = ({ value, label, id, ...props }) => {
   return (
     <BaseReviewDetailCard {...props}>
-      <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
-        {/* <Rating name="read-only" value={value} readOnly className="text-5xl" />
+      <Link href={`/reviewDetail/tertiary/school/${id}`}>
+        <a>
+          <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
+            {/* <Rating name="read-only" value={value} readOnly className="text-5xl" />
         <h1>{label}</h1> */}
-        <RantingTag
-          ranting={1}
-          title={`1.4 ${label}`}
-          className=" md:text-2xl text-lg md:px-4  font-black flex justify-center items-center"
-        />
-      </div>
+            <RantingTag
+              ranting={1}
+              title={`1.4 ${label}`}
+              className=" md:text-2xl text-lg md:px-4  font-black flex justify-center items-center"
+            />
+          </div>
+        </a>
+      </Link>
     </BaseReviewDetailCard>
   )
 }
