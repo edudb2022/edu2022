717053efc42f13cfa1551077df047add87335741
paulwonggogo <paul.wong@gogox.com>
2022-06-22 11:34:06 +0800

    Add review detail pages

--- client/pages/reviewDetail/tertiary/interview/[id].tsx
+++ client/pages/reviewDetail/tertiary/interview/[id].tsx
@@ -1,12 +1,11 @@
 import { NextPage } from "next"
 import React from "react"
-import RatingTag from "../../../../components/common/tags/rating"
 import DetailReviewHeaderContainer from "../../../../components/containers/detailReviewHeader"
 import DetailReviewInfoContainer from "../../../../components/containers/detailReviewInfo"
 
 import PageLayout from "../../../../components/layouts/page"
 
-const SchoolReviewDetailPage: NextPage = () => {
+const InterviewReviewDetailPage: NextPage = () => {
   return (
     <PageLayout>
       <DetailReviewHeaderContainer
@@ -16,13 +15,13 @@ const SchoolReviewDetailPage: NextPage = () => {
         schoolShortName="hku"
         ShortTitle="123"
       >
-        <RatingTag rating={5} title="12313" header="Overall" />
+        {/* <RatingTag rating={5} title="12313" header="Overall" />
         <div className="flex flex-wrap flex-around w-full border-2">
           <RatingTag rating={4} title="12313" header="Facility" />
           <RatingTag rating={1} title="12313" header="Policy" />
           <RatingTag rating={2} title="12313" header="Resources" />
           <RatingTag rating={3} title="12313" header="Canteen" />
-        </div>
+        </div> */}
 
         <DetailReviewInfoContainer
           admissionYear="2022"
@@ -36,4 +35,4 @@ const SchoolReviewDetailPage: NextPage = () => {
   )
 }
 
-export default SchoolReviewDetailPage
+export default InterviewReviewDetailPage
