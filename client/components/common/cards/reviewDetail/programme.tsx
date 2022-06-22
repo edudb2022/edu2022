6c3431239c02c2603809bbcd42ebc58a0b5e8425
paulwonggogo <paul.wong@gogox.com>
2022-06-22 11:33:32 +0800

    add link to review detail card

--- client/components/common/cards/reviewDetail/programme.tsx
+++ client/components/common/cards/reviewDetail/programme.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import React from "react"
 import BaseReviewDetailCard, { IBaseReviewDetailCardProps } from "."
 import { ID } from "../../../../types/common"
@@ -14,16 +15,20 @@ const ProgrammeReviewDetailCard: React.FunctionComponent<
 > = ({ value, label, id, ...props }) => {
   return (
     <BaseReviewDetailCard {...props}>
-      <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
-        {/* <Rating name="read-only" value={value} readOnly className="text-5xl" /> */}
-        <RatingTag
-          rating={3}
-          title={`${label}`}
-          // className="text-2xl font-black"
-          // ratingClassName="text-semi border-4"
-        />
-        {/* <h1>{label}</h1> */}
-      </div>
+      <Link href={`/reviewDetail/tertiary/programme/${id}`}>
+        <a>
+          <div className="flex flex-col-reverse md:flex-row h-full items-center justify-center gap-x-4">
+            {/* <Rating name="read-only" value={value} readOnly className="text-5xl" /> */}
+            <RatingTag
+              rating={3}
+              title={`${label}`}
+              // className="text-2xl font-black"
+              // ratingClassName="text-semi border-4"
+            />
+            {/* <h1>{label}</h1> */}
+          </div>
+        </a>
+      </Link>
     </BaseReviewDetailCard>
   )
 }
