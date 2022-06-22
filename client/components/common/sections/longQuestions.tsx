94212c3b614702a7af3e2f5f0038e879910adcb7
paulwonggogo <paul.wong@gogox.com>
2022-06-22 12:45:15 +0800

    add isDisplay prop to long question section

--- client/components/common/sections/longQuestions.tsx
+++ client/components/common/sections/longQuestions.tsx
@@ -1,24 +1,31 @@
-import React from "react";
-import BaseTextArea, { IBaseTextAreaProps } from "../inputs/textArea";
+import { Paper } from "@mui/material"
+import React from "react"
+import BaseTextArea, { IBaseTextAreaProps } from "../inputs/textArea"
 
-interface ILongQuestionsSectionProps extends IBaseTextAreaProps {
-  title: string;
+interface ILongQuestionsSectionProps extends Partial<IBaseTextAreaProps> {
+  title: string
+  isDisplay?: boolean
 }
 
 const LongQuestionsSection: React.FunctionComponent<
   ILongQuestionsSectionProps
-> = ({ title, className, isTouched, errorMessages, ...props }) => {
+> = ({ title, className, isTouched, errorMessages, isDisplay, ...props }) => {
   return (
     <div>
       <h1>{title}</h1>
-      <BaseTextArea
-        isTouched={isTouched}
-        errorMessages={errorMessages}
-        className={className}
-        {...props}
-      />
-    </div>
-  );
-};
 
-export default LongQuestionsSection;
+      {isDisplay ? (
+        <Paper>123</Paper>
+      ) : (
+        <BaseTextArea
+          isTouched={isTouched}
+          errorMessages={errorMessages}
+          className={className}
+          {...props}
+        />
+      )}
+    </div>
+  )
+}
+
+export default LongQuestionsSection
