66ee99184a1ef183c5d53c935bbaad6218926fab
paulwonggogo <paul.wong@gogox.com>
2022-06-23 14:14:23 +0800

    ,

--- client/components/footer/index.tsx
+++ client/components/footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer: React.FunctionComponent = () => {
-  return <footer className="border-2 w-full">footer</footer>
+  return <footer className="border-2 w-full bg-theme-two-500">footer</footer>
 }
 
 export default Footer
--- client/components/layouts/page.tsx
+++ client/components/layouts/page.tsx
@@ -7,13 +7,11 @@ const PageLayout: React.FunctionComponent<
   PropsWithChildren<PageLayoutProps>
 > = ({ children }) => {
   const router = useRouter()
-
+  // {
+  //   router.pathname === "/" ? "bg-theme-two-500" : "bg-white"
+  // }`}
   return (
-    <div
-      className={`flex justify-center w-full p-2  ${
-        router.pathname === "/" ? "bg-theme-two-500" : "bg-white"
-      }`}
-    >
+    <div className={`flex justify-center w-full p-2  bg-white`}>
       <div className="w-full md:w-9/10">{children}</div>
     </div>
   )
