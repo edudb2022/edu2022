2d00265e2ca54d55bf94948b03f0cd82797c4a5b
paulwonggogo <paul.wong@gogox.com>
2022-06-30 14:00:02 +0800

    .

---
+++ client/pages/PP.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PP = () => {
+  return <div>PP</div>
+}
+
+export default PP
---
+++ client/pages/TNC.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TNC = () => {
+  return <div>TNC</div>
+}
+
+export default TNC
