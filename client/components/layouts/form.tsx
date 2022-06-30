5c3f010b8f199a748befdcb01714f222e307ecca
paulwonggogo <paul.wong@gogox.com>
2022-06-30 14:08:31 +0800

    .

--- client/components/layouts/form.tsx
+++ client/components/layouts/form.tsx
@@ -5,7 +5,13 @@ interface FormPageLayoutProps {}
 const FormPageLayout: React.FunctionComponent<
   PropsWithChildren<FormPageLayoutProps>
 > = ({ children }) => {
-  return <div className="border-2  max-w-">children</div>
+  return (
+    <div className="flex flex-col justify-center items-center border-2  ">
+      <div className="flex min-w-[20rem] w-[80rem] border-2 border-green-500 ">
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default FormPageLayout
--- client/pages/forms/tertiary/schoolReview.tsx
+++ client/pages/forms/tertiary/schoolReview.tsx
@@ -3,7 +3,11 @@ import { EmailPasswordAuth } from "supertokens-auth-react/recipe/emailpassword"
 import FormPageLayout from "../../../components/layouts/form"
 
 const SchoolReviewFormPage: React.FunctionComponent = () => {
-  return <FormPageLayout>123</FormPageLayout>
+  return (
+    <FormPageLayout>
+      <div className="flex border-2 border-red-500">123</div>
+    </FormPageLayout>
+  )
 }
 
 export default SchoolReviewFormPage
