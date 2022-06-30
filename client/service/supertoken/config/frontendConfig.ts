1490612f7976d04dac4644fcefea211a17d44f0c
paulwonggogo <paul.wong@gogox.com>
2022-06-30 11:23:01 +0800

    Fixing frontend super token init problem

--- client/service/supertoken/config/frontendConfig.ts
+++ client/service/supertoken/config/frontendConfig.ts
@@ -2,9 +2,54 @@ import EmailPasswordReact from "supertokens-auth-react/recipe/emailpassword"
 import SessionReact from "supertokens-auth-react/recipe/session"
 import { appInfo } from "./appInfo"
 
+const abc = {
+  signInAndUpFeature: {
+    signUpForm: {
+      formFields: [
+        {
+          id: "name",
+          label: "Full name",
+          placeholder: "First name and last name"
+        },
+        {
+          id: "name1",
+          label: "Full nam1e",
+          placeholder: "First name and 1last name"
+        },
+        {
+          id: "name2",
+          label: "Full name3",
+          placeholder: "First name and last name4"
+        }
+      ],
+      termsOfServiceLink: "https://example.com/terms-of-service",
+      privacyPolicyLink: "https://example.com/privacy-policy"
+    },
+    signInForm: {
+      formFields: [
+        {
+          id: "name",
+          label: "Full name",
+          placeholder: "First name and last name"
+        },
+        {
+          id: "name1",
+          label: "Full nam1e",
+          placeholder: "First name and 1last name"
+        },
+        {
+          id: "name2",
+          label: "Full name3",
+          placeholder: "First name and last name4"
+        }
+      ]
+    }
+  }
+}
+
 export const frontendConfig = () => {
   return {
     appInfo,
-    recipeList: [EmailPasswordReact.init(), SessionReact.init()]
+    recipeList: [EmailPasswordReact.init(abc), SessionReact.init()]
   }
 }
--- client/service/supertoken/config/superToken.ts
+++ client/service/supertoken/config/superToken.ts
@@ -3,53 +3,53 @@ import EmailPassword from "supertokens-auth-react/recipe/emailpassword"
 import Session from "supertokens-auth-react/recipe/session"
 import { appInfo } from "./appInfo"
 
-SuperTokens.init({
-  appInfo: appInfo,
-  recipeList: [
-    EmailPassword.init({
-      signInAndUpFeature: {
-        signUpForm: {
-          formFields: [
-            {
-              id: "name",
-              label: "Full name",
-              placeholder: "First name and last name"
-            },
-            {
-              id: "name1",
-              label: "Full nam1e",
-              placeholder: "First name and 1last name"
-            },
-            {
-              id: "name2",
-              label: "Full name3",
-              placeholder: "First name and last name4"
-            }
-          ],
-          termsOfServiceLink: "https://example.com/terms-of-service",
-          privacyPolicyLink: "https://example.com/privacy-policy"
-        },
-        signInForm: {
-          formFields: [
-            {
-              id: "name",
-              label: "Full name",
-              placeholder: "First name and last name"
-            },
-            {
-              id: "name1",
-              label: "Full nam1e",
-              placeholder: "First name and 1last name"
-            },
-            {
-              id: "name2",
-              label: "Full name3",
-              placeholder: "First name and last name4"
-            }
-          ]
-        }
-      }
-    }),
-    Session.init()
-  ]
-})
+// SuperTokens.init({
+//   appInfo: appInfo,
+//   recipeList: [
+//     // EmailPassword.init({
+//     //   signInAndUpFeature: {
+//     //     signUpForm: {
+//     //       formFields: [
+//     //         {
+//     //           id: "name",
+//     //           label: "Full name",
+//     //           placeholder: "First name and last name"
+//     //         },
+//     //         {
+//     //           id: "name1",
+//     //           label: "Full nam1e",
+//     //           placeholder: "First name and 1last name"
+//     //         },
+//     //         {
+//     //           id: "name2",
+//     //           label: "Full name3",
+//     //           placeholder: "First name and last name4"
+//     //         }
+//     //       ],
+//     //       termsOfServiceLink: "https://example.com/terms-of-service",
+//     //       privacyPolicyLink: "https://example.com/privacy-policy"
+//     //     },
+//     //     signInForm: {
+//     //       formFields: [
+//     //         {
+//     //           id: "name",
+//     //           label: "Full name",
+//     //           placeholder: "First name and last name"
+//     //         },
+//     //         {
+//     //           id: "name1",
+//     //           label: "Full nam1e",
+//     //           placeholder: "First name and 1last name"
+//     //         },
+//     //         {
+//     //           id: "name2",
+//     //           label: "Full name3",
+//     //           placeholder: "First name and last name4"
+//     //         }
+//     //       ]
+//     //     }
+//     //   }
+//     // }),
+//     // Session.init()
+//   ]
+// })
