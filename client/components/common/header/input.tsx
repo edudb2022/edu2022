2dcbb7554a10e6a50eb2aa7b7c555c20f686e37b
Pjaijai <paul6a24@hotmail.com>
2022-07-02 11:56:44 +0800

    .

--- client/components/common/header/input.tsx
+++ client/components/common/header/input.tsx
@@ -4,15 +4,19 @@ import { boolean } from "yup"
 export interface IInputHeaderProps {
   header?: string
   headerRequired?: boolean
+  subHeader?: string
 }
 
-const InputHeader: React.FunctionComponent<
-  PropsWithChildren<IInputHeaderProps>
-> = ({ children, header, headerRequired }) => {
+const InputHeader: React.FunctionComponent<IInputHeaderProps> = ({
+  header,
+  headerRequired,
+  subHeader
+}) => {
   return (
     <div className="flex flex-row">
       {header && <h2>{header} </h2>}
       {headerRequired && <h2>* </h2>}
+      {subHeader && <h2>* </h2>}
     </div>
   )
 }
