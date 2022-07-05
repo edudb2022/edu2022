25cbb2ac4245103dddd3d31cc23485f8c735670e
Pjaijai <paul6a24@hotmail.com>
2022-07-05 22:05:01 +0800

    add st sessionto axio

--- client/utils/axios.ts
+++ client/utils/axios.ts
@@ -1,10 +1,11 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from "axios"
+import Session from "supertokens-auth-react/recipe/session"
 
 // import { auth } from "./firebase/configs";
 
 // DEV:
 export const DEFAULT_BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "";
+  process.env.NODE_ENV === "development" ? "http://localhost:3001/" : ""
 // process.env.GATSBY_API_URL;
 // const DEFAULT_BASE_URL = process.env.GATSBY_API_URL;
 
@@ -12,9 +13,11 @@ const instance: AxiosInstance = axios.create({
   baseURL: DEFAULT_BASE_URL,
   timeout: 5000,
   headers: {
-    "X-Custom-Header": "eduDB",
-  },
-});
+    "X-Custom-Header": "eduDB"
+  }
+})
+
+Session.addAxiosInterceptors(instance)
 
 // instance.interceptors.request.use(
 //   async function (config: any) {
@@ -34,13 +37,13 @@ instance.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    return response
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default instance;
+export default instance
