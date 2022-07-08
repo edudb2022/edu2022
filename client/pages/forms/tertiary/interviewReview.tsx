cba20db778e8549e43b6b25bb585d360e1c8b621
paulwonggogo <paul.wong@gogox.com>
2022-07-08 17:30:00 +0800

    .

---
+++ client/pages/forms/tertiary/interviewReview.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+import React from "react"
+
+const InterviewReviewPage: NextPage = () => {
+  return <div>InterviewReviewPage</div>
+}
+
+export default InterviewReviewPage
