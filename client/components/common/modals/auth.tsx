3db2d09f00cce1cf80578be2fccbea14b526fc53
Pjaijai <paul6a24@hotmail.com>
2022-07-23 13:46:50 +0800

    .

--- client/components/common/modals/auth.tsx
+++ client/components/common/modals/auth.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useDispatch } from "react-redux"
 import { useAppSelector } from "../../../hooks/common/useAppSelector"
-import { STRedirectToSignUp } from "../../../service/supertoken/function"
+import { STRedirectToSignIn } from "../../../service/supertoken/function"
 import { ISystemActionTypes } from "../../../state/system/actions"
 import ErrorIcon from "../icons/error"
 import DoubleButtonModal, { IDoubleButtonModalProps } from "./doubleButton"
@@ -19,7 +19,7 @@ const AuthModal: React.FunctionComponent<IAuthModalProps> = () => {
   }
 
   const handleRightClick = () => {
-    STRedirectToSignUp()
+    STRedirectToSignIn()
     dispatch({
       type: ISystemActionTypes.SYSTEM_IS_AUTH_MODAL_OPEN,
       payload: false
@@ -33,6 +33,7 @@ const AuthModal: React.FunctionComponent<IAuthModalProps> = () => {
       isOpen={isAuthModalOpen}
       onLeftClick={handleLeftClick}
       onRightCick={handleRightClick}
+      className=" h-48 w-80 md:h-56 md:w-96  "
     >
       <div className="flex flex-col justify-center items-center h-4/5 ">
         <span className="text-6xl text-theme-one-300">
--- client/components/common/modals/index.tsx
+++ client/components/common/modals/index.tsx
@@ -14,7 +14,7 @@ const BaseModal: React.FunctionComponent<
   return (
     <Modal open={isOpen} {...props}>
       <div
-        className={`absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white h-48 w-80 md:h-56 md:w-96  rounded-2xl outline-none ${className}`}
+        className={`absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl outline-none ${className}`}
       >
         {content && (
           <div className="w-full text-center p-2 border-b border-black  ">
