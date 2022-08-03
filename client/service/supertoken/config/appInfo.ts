608379a64626166fc7f7285407ce31aaf1d69318
Pjaijai <paul6a24@hotmail.com>
2022-08-03 21:05:06 +0800

    apiDomain

--- client/service/supertoken/config/appInfo.ts
+++ client/service/supertoken/config/appInfo.ts
@@ -1,7 +1,10 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
   appName: "OpenEduDB",
-  apiDomain: "http://localhost:3000",
+  apiDomain:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : `${process.env.NEXT_PUBLIC_BASE_URL}`,
   // websiteDomain:
   //   process.env.NODE_ENV === "development"
   //     ? "http://localhost:3000"
