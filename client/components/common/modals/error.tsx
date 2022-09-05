dec26c517a0e18960625df4dd157043e1106fd25
Pjaijai <paul6a24@hotmail.com>
2022-09-05 21:22:04 +0800

    add hanlde click to error modal

--- client/components/common/modals/error.tsx
+++ client/components/common/modals/error.tsx
@@ -1,28 +1,30 @@
 import React from "react"
 import ErrorIcon from "../icons/error"
-import SingleButtonModal from "./singleButton"
+import SingleButtonModal, { ISingleButtonModalProps } from "./singleButton"
 import { useAppSelector } from "../../../hooks/common/useAppSelector"
+import { useAppDispatch } from "../../../hooks/common/useAppDispatch"
+import { ISystemActionTypes } from "../../../state/system/actions"
 // import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-interface ISuccessModalProps {}
+interface ISuccessModalProps extends ISingleButtonModalProps {}
 
 const ErrorModal: React.FunctionComponent<ISuccessModalProps> = () => {
   const state = useAppSelector((state) => state.system.modals.error)
+  const dispatch = useAppDispatch()
 
   // const dispatch = useAppDispatch()
-  // const handleLeftClick = () => {
-  //   dispatch({
-  //     type: ISystemActionTypes.SYSTEM_IS_AUTH_MODAL_OPEN,
-  //     payload: false
-  //   })
-  // }
+  //   const handleLeftClick = () => {
+  //     dispatch({
+  //       type: ISystemActionTypes.SYSTEM_IS_AUTH_MODAL_OPEN,
+  //       payload: false
+  //     })
+  //   }
 
-  // const handleRightClick = () => {
-  //   STRedirectToSignIn()
-  //   dispatch({
-  //     type: ISystemActionTypes.SYSTEM_IS_AUTH_MODAL_OPEN,
-  //     payload: false
-  //   })
-  // }
+  const handleClick = () => {
+    dispatch({
+      type: ISystemActionTypes.SYSTEM_ERROR,
+      payload: ""
+    })
+  }
 
   console.log("222", state)
   return (
@@ -31,6 +33,7 @@ const ErrorModal: React.FunctionComponent<ISuccessModalProps> = () => {
       isOpen={!!state}
       buttonClassName="bg-theme-rose-100 text-theme-rose-700 font-semibold"
       className=" h-48 w-80 md:h-56 md:w-96 "
+      onClick={handleClick}
     >
       <div className="flex flex-col justify-center items-center h-40">
         <span className="text-8xl text-theme-rose-600">
