ea70cb4574359fde12c283bf65b62c6d300261c1
Pjaijai <paul6a24@hotmail.com>
2022-09-23 11:34:32 +0800

    rating question

---
+++ client/constants/ratingQuestion.ts
@@ -0,0 +1,12 @@
+export const schoolReviewratingQuestions = {
+  [1]: { id: 1, question: "學校設施及環境" },
+  [2]: {
+    id: 2,
+    question: "資源及支援",
+    helpertext: "例如Scholarship數量，Exchange支援，就業及情緒輔導等等"
+  },
+  [3]: { id: 3, question: "學校政策" },
+  [4]: { id: 4, question: "校內膳食" },
+  [5]: { id: 5, question: "歸屬感" },
+  [6]: { id: 6, question: "推薦指數" }
+}
