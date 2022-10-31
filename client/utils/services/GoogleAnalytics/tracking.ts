69a09385f2c5ae4d14823a0772ebd01471fcc840
Pjaijai <paul6a24@hotmail.com>
2022-10-31 21:49:05 +0800

    Add costum tracking event

--- client/utils/services/GoogleAnalytics/tracking.ts
+++ client/utils/services/GoogleAnalytics/tracking.ts
@@ -1,3 +1,5 @@
+// import ReactGA from "react-ga"
+// ReactGA.initialize(`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`)
 const trackingEvent = {
   pageView: (url: string) => {
     window.gtag(
@@ -7,10 +9,22 @@ const trackingEvent = {
     )
   },
 
-  pageTypeView: (eventCategory: string, eventlabel: string, value: number) => {
-    window.gtag("event", "pageVi122323232323232323123ew", {
-      event_category: eventCategory,
-      event_label: eventlabel,
+  // pageTypeView: (eventCategory: string, eventlabel: string, value: number) => {
+  //   window.gtag("event", "pageVi122323232323232323123ew", {
+  //     event_category: eventCategory,
+  //     event_label: eventlabel,
+  //     value: value
+  //   })
+  // },
+  customEvent: (
+    action: string,
+    event_category: string,
+    event_label: string,
+    value: number
+  ) => {
+    window.gtag("event", action, {
+      event_category: event_category,
+      event_label: event_label,
       value: value
     })
   }
