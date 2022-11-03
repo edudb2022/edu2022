82e3986e3d4ff6ea6af5b0e0cc54674ff7aa022d
Pjaijai <paul6a24@hotmail.com>
2022-11-03 22:22:40 +0800

    Rename type

--- client/constants/rating.ts
+++ client/constants/rating.ts
@@ -1,167 +1,167 @@
 export interface IRating {
-  value: string
+  value: number
   label: string
 }
 
 export const programmeLearningExprienceRating: IRating[] = [
-  { value: "1", label: "極之垃圾" },
-  { value: "2", label: "垃圾" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾好" },
-  { value: "5", label: "良好" },
-  { value: "6", label: "Firm啊!" }
+  { value: 1, label: "極之垃圾" },
+  { value: 2, label: "垃圾" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾好" },
+  { value: 5, label: "良好" },
+  { value: 6, label: "Firm啊!" }
 ]
 export const recommendRating: IRating[] = [
-  { value: "1", label: "極之垃圾" },
-  { value: "2", label: "垃圾" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾好" },
-  { value: "5", label: "良好" },
-  { value: "6", label: "Firm啊!" }
+  { value: 1, label: "極之垃圾" },
+  { value: 2, label: "垃圾" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾好" },
+  { value: 5, label: "良好" },
+  { value: 6, label: "Firm啊!" }
 ]
 //school
 export const schoolCampusRating: IRating[] = [
-  { value: "1", label: "垃圾" },
-  { value: "2", label: "好差" },
-  { value: "3", label: "強差人意" },
-  { value: "4", label: "不過不失" },
-  { value: "5", label: "良好" },
-  { value: "6", label: "完美" }
+  { value: 1, label: "垃圾" },
+  { value: 2, label: "好差" },
+  { value: 3, label: "強差人意" },
+  { value: 4, label: "不過不失" },
+  { value: 5, label: "良好" },
+  { value: 6, label: "完美" }
 ]
 
 export const schoolResourcesRating: IRating[] = [
-  { value: "1", label: "0資源" },
-  { value: "2", label: "極少" },
-  { value: "3", label: "啱啱好" },
-  { value: "4", label: "不錯 " },
-  { value: "5", label: "極多" },
-  { value: "6", label: "極度豐富" }
+  { value: 1, label: "0資源" },
+  { value: 2, label: "極少" },
+  { value: 3, label: "啱啱好" },
+  { value: 4, label: "不錯 " },
+  { value: 5, label: "極多" },
+  { value: 6, label: "極度豐富" }
 ]
 
 export const schoolPolicyRating: IRating[] = [
-  { value: "1", label: "離地" },
-  { value: "2", label: "垃圾" },
-  { value: "3", label: "頗差" },
-  { value: "4", label: "不錯" },
-  { value: "5", label: "大致上合理" },
-  { value: "6", label: "貼地" }
+  { value: 1, label: "離地" },
+  { value: 2, label: "垃圾" },
+  { value: 3, label: "頗差" },
+  { value: 4, label: "不錯" },
+  { value: 5, label: "大致上合理" },
+  { value: 6, label: "貼地" }
 ]
 
 export const shcoolCafeteriaRating: IRating[] = [
-  { value: "1", label: "食物沙漠" },
-  { value: "2", label: "冇乜野食" },
-  { value: "3", label: "都叫有得揀" },
-  { value: "4", label: "不錯" },
-  { value: "5", label: "很好" },
-  { value: "6", label: "選擇多質素好" }
+  { value: 1, label: "食物沙漠" },
+  { value: 2, label: "冇乜野食" },
+  { value: 3, label: "都叫有得揀" },
+  { value: 4, label: "不錯" },
+  { value: 5, label: "很好" },
+  { value: 6, label: "選擇多質素好" }
 ]
 export const schoolSelfOfBelongingRating: IRating[] = [
-  { value: "1", label: "極反感" },
-  { value: "2", label: "少反感" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "少量" },
-  { value: "5", label: "感覺良好" },
-  { value: "6", label: "極強烈" }
+  { value: 1, label: "極反感" },
+  { value: 2, label: "少反感" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "少量" },
+  { value: 5, label: "感覺良好" },
+  { value: 6, label: "極強烈" }
 ]
 
 export const schoolRecommendRating: IRating[] = [
-  { value: "1", label: "讀咗會後悔" },
-  { value: "2", label: "好差" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾好嘅" },
-  { value: "5", label: "幾推薦" },
-  { value: "6", label: "極度推薦" }
+  { value: 1, label: "讀咗會後悔" },
+  { value: 2, label: "好差" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾好嘅" },
+  { value: 5, label: "幾推薦" },
+  { value: 6, label: "極度推薦" }
 ]
 
 //programme
 export const programmeStructureRating: IRating[] = [
-  { value: "1", label: "極之垃圾" },
-  { value: "2", label: "垃圾" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾好" },
-  { value: "5", label: "良好" },
-  { value: "6", label: "完美" }
+  { value: 1, label: "極之垃圾" },
+  { value: 2, label: "垃圾" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾好" },
+  { value: 5, label: "良好" },
+  { value: 6, label: "完美" }
 ]
 
 export const programmeGpaRating: IRating[] = [
-  { value: "1", label: "爛到Sir" },
-  { value: "2", label: "爛爛地" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "良好" },
-  { value: "5", label: "算係咁" },
-  { value: "6", label: "靚龜到Sir" }
+  { value: 1, label: "爛到Sir" },
+  { value: 2, label: "爛爛地" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "良好" },
+  { value: 5, label: "算係咁" },
+  { value: 6, label: "靚龜到Sir" }
 ]
 
 export const progrmmeWorkLoadRating: IRating[] = [
-  { value: "1", label: "爆兩個肝" },
-  { value: "2", label: "爆一個肝" },
-  { value: "3", label: "正常" },
-  { value: "4", label: "算舒服" },
-  { value: "5", label: "好Chill" },
-  { value: "6", label: "養肝Mode" }
+  { value: 1, label: "爆兩個肝" },
+  { value: 2, label: "爆一個肝" },
+  { value: 3, label: "正常" },
+  { value: 4, label: "算舒服" },
+  { value: 5, label: "好Chill" },
+  { value: 6, label: "養肝Mode" }
 ]
 
 export const progrmmeResourcedRating: IRating[] = [
-  { value: "1", label: "0資源" },
-  { value: "2", label: "極少" },
-  { value: "3", label: "啱啱好" },
-  { value: "4", label: "不錯 " },
-  { value: "5", label: "極多" },
-  { value: "6", label: "極度豐富" }
+  { value: 1, label: "0資源" },
+  { value: 2, label: "極少" },
+  { value: 3, label: "啱啱好" },
+  { value: 4, label: "不錯 " },
+  { value: 5, label: "極多" },
+  { value: 6, label: "極度豐富" }
 ]
 
 export const progrmmeRecommendRating: IRating[] = [
-  { value: "1", label: "遠離呢科" },
-  { value: "2", label: "三思而後行" },
-  { value: "3", label: "想讀就讀" },
-  { value: "4", label: "可以一戰" },
-  { value: "5", label: "力推！" },
-  { value: "6", label: "唔讀就傻仔啦" }
+  { value: 1, label: "遠離呢科" },
+  { value: 2, label: "三思而後行" },
+  { value: 3, label: "想讀就讀" },
+  { value: 4, label: "可以一戰" },
+  { value: 5, label: "力推！" },
+  { value: 6, label: "唔讀就傻仔啦" }
 ]
 
 //interview
 
 export const interviewExperienceRating: IRating[] = [
-  { value: "1", label: "極惡劣" },
-  { value: "2", label: "好差" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾好" },
-  { value: "5", label: "良好" },
-  { value: "6", label: "十分好" }
+  { value: 1, label: "極惡劣" },
+  { value: 2, label: "好差" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾好" },
+  { value: 5, label: "良好" },
+  { value: 6, label: "十分好" }
 ]
 
 export const interviewDifficultyRating: IRating[] = [
-  { value: "1", label: "0識" },
-  { value: "2", label: "好難" },
-  { value: "3", label: "普通" },
-  { value: "4", label: "幾輕鬆" },
-  { value: "5", label: "輕鬆" },
-  { value: "6", label: "太易了" }
+  { value: 1, label: "0識" },
+  { value: 2, label: "好難" },
+  { value: 3, label: "普通" },
+  { value: 4, label: "幾輕鬆" },
+  { value: 5, label: "輕鬆" },
+  { value: 6, label: "太易了" }
 ]
 
 export const intershipJobFindingDifficultyRating: IRating[] = [
-  { value: "1", label: "準備乞食" },
-  { value: "2", label: "困難" },
-  { value: "3", label: "正常" },
-  { value: "4", label: "幾易" },
-  { value: "5", label: "輕鬆" },
-  { value: "6", label: "一定有工開！" }
+  { value: 1, label: "準備乞食" },
+  { value: 2, label: "困難" },
+  { value: 3, label: "正常" },
+  { value: 4, label: "幾易" },
+  { value: 5, label: "輕鬆" },
+  { value: 6, label: "一定有工開！" }
 ]
 
 export const gradJobJobFindingDifficultyRating: IRating[] = [
-  { value: "1", label: "準備乞食" },
-  { value: "2", label: "困難" },
-  { value: "3", label: "正常" },
-  { value: "4", label: "幾易" },
-  { value: "5", label: "輕鬆" },
-  { value: "6", label: "一定有工開！" }
+  { value: 1, label: "準備乞食" },
+  { value: 2, label: "困難" },
+  { value: 3, label: "正常" },
+  { value: 4, label: "幾易" },
+  { value: 5, label: "輕鬆" },
+  { value: 6, label: "一定有工開！" }
 ]
 
 export const gradJobHopeRating: IRating[] = [
-  { value: "1", label: "0希望" },
-  { value: "2", label: "前路崎嶇" },
-  { value: "3", label: "正常" },
-  { value: "4", label: "ok啦" },
-  { value: "5", label: "有希望" },
-  { value: "6", label: "急不及待好好玩玩了" }
+  { value: 1, label: "0希望" },
+  { value: 2, label: "前路崎嶇" },
+  { value: 3, label: "正常" },
+  { value: 4, label: "ok啦" },
+  { value: 5, label: "有希望" },
+  { value: 6, label: "急不及待好好玩玩了" }
 ]
