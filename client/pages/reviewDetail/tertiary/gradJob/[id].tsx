41c06112e1cd46156d0f468cf8d2e312d0569574
Pjaijai <paul6a24@hotmail.com>
2022-11-06 20:02:22 +0800

    Fix build failed problem

--- client/pages/reviewDetail/tertiary/gradJob/[id].tsx
+++ client/pages/reviewDetail/tertiary/gradJob/[id].tsx
@@ -18,6 +18,7 @@ const GradJobReviewDetailPage: NextPage = () => {
         EnglishTitle="123"
         schoolShortName="hku"
         ShortTitle="123"
+        onVote={() => {}}
       >
         <div className="flex flex-wrap flex-row justify-evenly w-full border-2">
           <RatingTag rating={5} title="12313" header="搵工難度" />
