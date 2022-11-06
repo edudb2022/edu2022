ba958fba155e0dfd4c1ac1d8765c076e2f448264
Pjaijai <paul6a24@hotmail.com>
2022-11-06 19:36:36 +0800

    Fix use schoolvote

--- client/hooks/api/vote/useVoteSchoolReview.tsx
+++ client/hooks/api/vote/useVoteSchoolReview.tsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query"
 import { apiService } from "../../../utils/api/api"
 
 const useVoteSchoolReview = () => {
-  return useMutation(apiService.voteProgrammeReview)
+  return useMutation(apiService.voteSchoolReview)
 }
 
 export default useVoteSchoolReview
