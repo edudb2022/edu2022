fdda1b006d7994ab03d7dec17b0d64f8c3574ead
Pjaijai <paul6a24@hotmail.com>
2022-11-16 23:13:55 +0800

    Create job cat  mapper

---
+++ client/mappers/jobCategory.ts
@@ -0,0 +1,180 @@
+import { JobCategoryTypeId } from "../types/common"
+
+export const JobCategoryMapper: any = {
+  [JobCategoryTypeId.ARCHITECTURE]: {
+    englishDisplayText: "Architecture",
+    id: 1,
+    chineseDisplayText: "建築"
+  },
+  [JobCategoryTypeId.ARTS_AND_CREATION]: {
+    englishDisplayText: "Arts and creation",
+    id: 2,
+    chineseDisplayText: "藝術及創作"
+  },
+  [JobCategoryTypeId.CUSTOMER_SERVICE]: {
+    englishDisplayText: "Customer service",
+    id: 3,
+    chineseDisplayText: "客戶服務"
+  },
+  [JobCategoryTypeId.DESIGN]: {
+    englishDisplayText: "Design",
+    id: 4,
+    chineseDisplayText: "設計"
+  },
+  [JobCategoryTypeId.EDUCATION]: {
+    englishDisplayText: "Education",
+    id: 5,
+    chineseDisplayText: "教育"
+  },
+  [JobCategoryTypeId.ENGINEERING]: {
+    englishDisplayText: "Engineering",
+    id: 6,
+    chineseDisplayText: "工程"
+  },
+  [JobCategoryTypeId.FILM]: {
+    englishDisplayText: "Film",
+    id: 7,
+    chineseDisplayText: "電影"
+  },
+  [JobCategoryTypeId.FINANCE]: {
+    englishDisplayText: "Finance",
+    id: 8,
+    chineseDisplayText: "金融"
+  },
+  [JobCategoryTypeId.FOOD_AND_BEVERAGES]: {
+    englishDisplayText: "Food and Beverages",
+    id: 9,
+    chineseDisplayText: "飲食"
+  },
+  [JobCategoryTypeId.HEALTH]: {
+    englishDisplayText: "Health",
+    id: 10,
+    chineseDisplayText: "健康"
+  },
+  [JobCategoryTypeId.HUMAN_RESOURCE]: {
+    englishDisplayText: "Human Resource",
+    id: 11,
+
+    chineseDisplayText: "人力資源"
+  },
+  [JobCategoryTypeId.INSURANCE]: {
+    englishDisplayText: "Insurance",
+    id: 12,
+
+    chineseDisplayText: "保險"
+  },
+  [JobCategoryTypeId.LEGAL_SERVICE]: {
+    englishDisplayText: "Legal Service",
+    id: 13,
+
+    chineseDisplayText: "法律服務"
+  },
+  [JobCategoryTypeId.LOGISTIC]: {
+    englishDisplayText: "Logistic",
+    id: 14,
+
+    chineseDisplayText: "物流"
+  },
+  [JobCategoryTypeId.JEWELRY]: {
+    englishDisplayText: "Jewelry",
+    id: 15,
+
+    chineseDisplayText: "珠寶"
+  },
+  [JobCategoryTypeId.MANAGEMENT]: {
+    englishDisplayText: "Management",
+    id: 16,
+
+    chineseDisplayText: "管理"
+  },
+  [JobCategoryTypeId.MANUFACTURE]: {
+    englishDisplayText: "Manufacture",
+    id: 17,
+
+    chineseDisplayText: "製造業"
+  },
+  [JobCategoryTypeId.MARKETING]: {
+    englishDisplayText: "Marketing",
+    id: 18,
+
+    chineseDisplayText: "市場推廣"
+  },
+  [JobCategoryTypeId.MEDIA]: {
+    englishDisplayText: "Media",
+    id: 19,
+
+    chineseDisplayText: "媒體"
+  },
+  [JobCategoryTypeId.MEDICAL_SERVICE]: {
+    englishDisplayText: "Medical Service",
+    id: 20,
+
+    chineseDisplayText: "醫療服務"
+  },
+  [JobCategoryTypeId.PRIMARY_INDUSTRY]: {
+    englishDisplayText: "Primary industry",
+    id: 21,
+
+    chineseDisplayText: "漁農業"
+  },
+  [JobCategoryTypeId.PROPERTY]: {
+    englishDisplayText: "Property",
+    id: 22,
+
+    chineseDisplayText: "房地產"
+  },
+  [JobCategoryTypeId.RETAIL]: {
+    englishDisplayText: "Retail",
+    id: 23,
+
+    chineseDisplayText: "零售"
+  },
+  [JobCategoryTypeId.SCIENCE]: {
+    englishDisplayText: "Science",
+    id: 24,
+
+    chineseDisplayText: "科學"
+  },
+  [JobCategoryTypeId.SOCIAL_SERVICE]: {
+    englishDisplayText: "Social service",
+    id: 25,
+
+    chineseDisplayText: "社會服務"
+  },
+  [JobCategoryTypeId.TEXTILES]: {
+    englishDisplayText: "Textiles",
+    id: 26,
+
+    chineseDisplayText: "紡織品"
+  },
+  [JobCategoryTypeId.TECHNOLOGY]: {
+    englishDisplayText: "Technology",
+    id: 27,
+
+    chineseDisplayText: "科技"
+  },
+  [JobCategoryTypeId.TOURISM_AND_TRAVEL]: {
+    englishDisplayText: "Tourism and travel",
+    id: 28,
+
+    chineseDisplayText: "旅遊業"
+  },
+  [JobCategoryTypeId.TRANSLATION]: {
+    englishDisplayText: "Translation",
+    id: 29,
+
+    chineseDisplayText: "翻譯"
+  },
+  [JobCategoryTypeId.UNCATEGORIZED]: {
+    englishDisplayText: "Uncategorized",
+    id: 30,
+
+    chineseDisplayText: "未分類"
+  },
+  [JobCategoryTypeId.OTHER]: {
+    englishDisplayText: "Other",
+    id: 31,
+
+    chineseDisplayText: "其他"
+  }
+}
