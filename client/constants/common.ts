c2303ea5f12b2f71e713c65097eb504a2a086beb
Pjaijai <paul6a24@hotmail.com>
2022-11-16 23:14:24 +0800

    list

--- client/constants/common.ts
+++ client/constants/common.ts
@@ -451,6 +451,890 @@ const gradJobOfferSortingTypesList = [
   }
 ]
 
+const jobTypeList = [
+  {
+    id: 1,
+    guiOrder: 0,
+    chineseDisplayText: "建築",
+    englishDisplayText: "Architecture",
+    JobCategoryId: 1
+  },
+  {
+    id: 200,
+    guiOrder: 1,
+    chineseDisplayText: "內容創作者",
+    englishDisplayText: "Content Creator",
+    JobCategoryId: 2
+  },
+  {
+    id: 201,
+    guiOrder: 2,
+    chineseDisplayText: "娛樂",
+    englishDisplayText: "Entertainment",
+    JobCategoryId: 2
+  },
+  {
+    id: 202,
+    guiOrder: 3,
+    chineseDisplayText: "音樂",
+    englishDisplayText: "Music",
+    JobCategoryId: 2
+  },
+  {
+    id: 203,
+    guiOrder: 4,
+    chineseDisplayText: "表演者",
+    englishDisplayText: "Performer",
+    JobCategoryId: 2
+  },
+  {
+    id: 204,
+    guiOrder: 5,
+    chineseDisplayText: "寫作和編輯",
+    englishDisplayText: "Writing And Editing",
+    JobCategoryId: 2
+  },
+  {
+    id: 299,
+    guiOrder: 6,
+    chineseDisplayText: "藝術與創作 (其他)",
+    englishDisplayText: "Arts And Creation (Other)",
+    JobCategoryId: 2
+  },
+  {
+    id: 300,
+    guiOrder: 7,
+    chineseDisplayText: "客戶服務",
+    englishDisplayText: "Customer Service",
+    JobCategoryId: 3
+  },
+  {
+    id: 400,
+    guiOrder: 8,
+    chineseDisplayText: "時裝設計",
+    englishDisplayText: "Fashion Design",
+    JobCategoryId: 4
+  },
+  {
+    id: 401,
+    guiOrder: 9,
+    chineseDisplayText: "平面設計",
+    englishDisplayText: "Graphics Design",
+    JobCategoryId: 4
+  },
+  {
+    id: 402,
+    guiOrder: 10,
+    chineseDisplayText: "室內設計",
+    englishDisplayText: "Interior Design",
+    JobCategoryId: 4
+  },
+  {
+    id: 403,
+    guiOrder: 11,
+    chineseDisplayText: "產品設計",
+    englishDisplayText: "Product Design",
+    JobCategoryId: 4
+  },
+  {
+    id: 404,
+    guiOrder: 12,
+    chineseDisplayText: "UXUI設計",
+    englishDisplayText: "Uxui Design",
+    JobCategoryId: 4
+  },
+  {
+    id: 499,
+    guiOrder: 13,
+    chineseDisplayText: "設計（其他）",
+    englishDisplayText: "Design (Other)",
+    JobCategoryId: 4
+  },
+  {
+    id: 500,
+    guiOrder: 14,
+    chineseDisplayText: "幼兒園老師",
+    englishDisplayText: "Kindergarten Teacher",
+    JobCategoryId: 5
+  },
+  {
+    id: 501,
+    guiOrder: 15,
+    chineseDisplayText: "講師或教授",
+    englishDisplayText: "Lecturer Or Professor",
+    JobCategoryId: 5
+  },
+  {
+    id: 502,
+    guiOrder: 16,
+    chineseDisplayText: "小學教師",
+    englishDisplayText: "Primary School Teacher",
+    JobCategoryId: 5
+  },
+  {
+    id: 503,
+    guiOrder: 17,
+    chineseDisplayText: "中學教師",
+    englishDisplayText: "Secondary School Teacher",
+    JobCategoryId: 5
+  },
+  {
+    id: 504,
+    guiOrder: 18,
+    chineseDisplayText: "導師",
+    englishDisplayText: "Tutor",
+    JobCategoryId: 5
+  },
+  {
+    id: 599,
+    guiOrder: 19,
+    chineseDisplayText: "教育（其他）",
+    englishDisplayText: "Education (Other)",
+    JobCategoryId: 5
+  },
+  {
+    id: 600,
+    guiOrder: 20,
+    chineseDisplayText: "屋宇裝備工程",
+    englishDisplayText: "Building Services Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 601,
+    guiOrder: 21,
+    chineseDisplayText: "土木與結構工程",
+    englishDisplayText: "Civil And Structural Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 602,
+    guiOrder: 22,
+    chineseDisplayText: "化學工程",
+    englishDisplayText: "Chemical Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 603,
+    guiOrder: 23,
+    chineseDisplayText: "繪圖員",
+    englishDisplayText: "Draftsman",
+    JobCategoryId: 6
+  },
+  {
+    id: 604,
+    guiOrder: 24,
+    chineseDisplayText: "機電工程",
+    englishDisplayText: "Electrical Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 605,
+    guiOrder: 25,
+    chineseDisplayText: "電子工程",
+    englishDisplayText: "Electronic Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 606,
+    guiOrder: 26,
+    chineseDisplayText: "機械工業",
+    englishDisplayText: "Mechanical Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 607,
+    guiOrder: 27,
+    chineseDisplayText: "網路工程",
+    englishDisplayText: "Network Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 608,
+    guiOrder: 28,
+    chineseDisplayText: "半導體工程",
+    englishDisplayText: "Semiconductor Engineering",
+    JobCategoryId: 6
+  },
+  {
+    id: 609,
+    guiOrder: 29,
+    chineseDisplayText: "測量",
+    englishDisplayText: "Surveying",
+    JobCategoryId: 6
+  },
+  {
+    id: 699,
+    guiOrder: 30,
+    chineseDisplayText: "工程（其他）",
+    englishDisplayText: "Engineering (Other)",
+    JobCategoryId: 6
+  },
+  {
+    id: 700,
+    guiOrder: 31,
+    chineseDisplayText: "導演",
+    englishDisplayText: "Director",
+    JobCategoryId: 7
+  },
+  {
+    id: 701,
+    guiOrder: 32,
+    chineseDisplayText: "電影製作",
+    englishDisplayText: "Film Prodution",
+    JobCategoryId: 7
+  },
+  {
+    id: 702,
+    guiOrder: 33,
+    chineseDisplayText: "燈光師",
+    englishDisplayText: "Lighting Technician",
+    JobCategoryId: 7
+  },
+  {
+    id: 703,
+    guiOrder: 34,
+    chineseDisplayText: "音效師",
+    englishDisplayText: "Sound Technician",
+    JobCategoryId: 7
+  },
+  {
+    id: 799,
+    guiOrder: 35,
+    chineseDisplayText: "電影（其他）",
+    englishDisplayText: "Film (Other)",
+    JobCategoryId: 7
+  },
+  {
+    id: 800,
+    guiOrder: 36,
+    chineseDisplayText: "會計",
+    englishDisplayText: "Accounting",
+    JobCategoryId: 8
+  },
+  {
+    id: 801,
+    guiOrder: 37,
+    chineseDisplayText: "審計",
+    englishDisplayText: "Audit",
+    JobCategoryId: 8
+  },
+  {
+    id: 802,
+    guiOrder: 38,
+    chineseDisplayText: "銀行",
+    englishDisplayText: "Banking",
+    JobCategoryId: 8
+  },
+  {
+    id: 803,
+    guiOrder: 39,
+    chineseDisplayText: "業務分析師",
+    englishDisplayText: "Business Analyst",
+    JobCategoryId: 8
+  },
+  {
+    id: 804,
+    guiOrder: 40,
+    chineseDisplayText: "金融",
+    englishDisplayText: "Finance",
+    JobCategoryId: 8
+  },
+  {
+    id: 899,
+    guiOrder: 41,
+    chineseDisplayText: "金融（其他）",
+    englishDisplayText: "finance (Other)",
+    JobCategoryId: 8
+  },
+  {
+    id: 900,
+    guiOrder: 42,
+    chineseDisplayText: "中菜",
+    englishDisplayText: "Chinese Cuisine",
+    JobCategoryId: 9
+  },
+  {
+    id: 901,
+    guiOrder: 43,
+    chineseDisplayText: "西菜",
+    englishDisplayText: "Western Cuisine",
+    JobCategoryId: 9
+  },
+  {
+    id: 999,
+    guiOrder: 44,
+    chineseDisplayText: "飲食(其他)",
+    englishDisplayText: "Food And Beverages (Other)",
+    JobCategoryId: 9
+  },
+  {
+    id: 1000,
+    guiOrder: 45,
+    chineseDisplayText: "美容化妝品和護理",
+    englishDisplayText: "Beauty Cosmetics And Care",
+    JobCategoryId: 10
+  },
+  {
+    id: 1001,
+    guiOrder: 46,
+    chineseDisplayText: "健康",
+    englishDisplayText: "Fitness",
+    JobCategoryId: 10
+  },
+  {
+    id: 1002,
+    guiOrder: 47,
+    chineseDisplayText: "運動",
+    englishDisplayText: "Sport",
+    JobCategoryId: 10
+  },
+  {
+    id: 1099,
+    guiOrder: 48,
+    chineseDisplayText: "健康（其他）",
+    englishDisplayText: "Health (Other)",
+    JobCategoryId: 10
+  },
+  {
+    id: 1100,
+    guiOrder: 49,
+    chineseDisplayText: "員工和招聘",
+    englishDisplayText: "Staffing And Recruiting",
+    JobCategoryId: 11
+  },
+  {
+    id: 1101,
+    guiOrder: 50,
+    chineseDisplayText: "求職顧問",
+    englishDisplayText: "Recruitment Counsultant",
+    JobCategoryId: 11
+  },
+  {
+    id: 1199,
+    guiOrder: 51,
+    chineseDisplayText: "人力資源（其他）",
+    englishDisplayText: "Human Resource (Other)",
+    JobCategoryId: 11
+  },
+  {
+    id: 1200,
+    guiOrder: 52,
+    chineseDisplayText: "保險",
+    englishDisplayText: "Insurance",
+    JobCategoryId: 12
+  },
+  {
+    id: 1300,
+    guiOrder: 53,
+    chineseDisplayText: "大律師",
+    englishDisplayText: "Barrister",
+    JobCategoryId: 13
+  },
+  {
+    id: 1301,
+    guiOrder: 54,
+    chineseDisplayText: "法律實習生",
+    englishDisplayText: "Legal Trainee",
+    JobCategoryId: 13
+  },
+  {
+    id: 1302,
+    guiOrder: 55,
+    chineseDisplayText: "法律顧問",
+    englishDisplayText: "Legal Consultant",
+    JobCategoryId: 13
+  },
+  {
+    id: 1303,
+    guiOrder: 56,
+    chineseDisplayText: "事務律師",
+    englishDisplayText: "Solicitor",
+    JobCategoryId: 13
+  },
+  {
+    id: 1399,
+    guiOrder: 57,
+    chineseDisplayText: "法律服務（其他）",
+    englishDisplayText: "Legal Services (Other)",
+    JobCategoryId: 13
+  },
+  {
+    id: 1400,
+    guiOrder: 58,
+    chineseDisplayText: "運輸和物流",
+    englishDisplayText: "Transportation And Logistic",
+    JobCategoryId: 14
+  },
+  {
+    id: 1500,
+    guiOrder: 59,
+    chineseDisplayText: "奢侈品和珠寶",
+    englishDisplayText: "Luxury Goods And Jewelry",
+    JobCategoryId: 15
+  },
+  {
+    id: 1600,
+    guiOrder: 60,
+    chineseDisplayText: "項目經理",
+    englishDisplayText: "Project Manager",
+    JobCategoryId: 16
+  },
+  {
+    id: 1601,
+    guiOrder: 61,
+    chineseDisplayText: "管理培訓生",
+    englishDisplayText: "Management Trainee",
+    JobCategoryId: 16
+  },
+  {
+    id: 1699,
+    guiOrder: 62,
+    chineseDisplayText: "管理（其他）",
+    englishDisplayText: "Management (other)",
+    JobCategoryId: 16
+  },
+  {
+    id: 1700,
+    guiOrder: 63,
+    chineseDisplayText: "製造業",
+    englishDisplayText: "Manufacturing",
+    JobCategoryId: 17
+  },
+  {
+    id: 1800,
+    guiOrder: 64,
+    chineseDisplayText: "廣告",
+    englishDisplayText: "Advertising",
+    JobCategoryId: 18
+  },
+  {
+    id: 1801,
+    guiOrder: 65,
+    chineseDisplayText: "數位市場推廣策劃",
+    englishDisplayText: "Digital Marketing",
+    JobCategoryId: 18
+  },
+  {
+    id: 1802,
+    guiOrder: 66,
+    chineseDisplayText: "市場推廣策劃",
+    englishDisplayText: "Marketing",
+    JobCategoryId: 18
+  },
+  {
+    id: 1899,
+    guiOrder: 67,
+    chineseDisplayText: "市場推廣策劃 (其他)",
+    englishDisplayText: "Marketing (Other)",
+    JobCategoryId: 18
+  },
+  {
+    id: 1900,
+    guiOrder: 68,
+    chineseDisplayText: "廣播",
+    englishDisplayText: "Broadcasting",
+    JobCategoryId: 19
+  },
+  {
+    id: 1901,
+    guiOrder: 69,
+    chineseDisplayText: "媒體製作",
+    englishDisplayText: "Media Production",
+    JobCategoryId: 19
+  },
+  {
+    id: 1902,
+    guiOrder: 70,
+    chineseDisplayText: "多媒體",
+    englishDisplayText: "Multi Media",
+    JobCategoryId: 19
+  },
+  {
+    id: 1903,
+    guiOrder: 71,
+    chineseDisplayText: "編輯和新聞",
+    englishDisplayText: "Editor And Journalism",
+    JobCategoryId: 19
+  },
+  {
+    id: 1999,
+    guiOrder: 72,
+    chineseDisplayText: "媒體（其他）",
+    englishDisplayText: "Media (Other)",
+    JobCategoryId: 19
+  },
+  {
+    id: 2000,
+    guiOrder: 73,
+    chineseDisplayText: "醫生",
+    englishDisplayText: "Doctor",
+    JobCategoryId: 20
+  },
+  {
+    id: 2001,
+    guiOrder: 74,
+    chineseDisplayText: "音樂治療師",
+    englishDisplayText: "Music Therapist",
+    JobCategoryId: 20
+  },
+  {
+    id: 2002,
+    guiOrder: 75,
+    chineseDisplayText: "護士",
+    englishDisplayText: "Nurse",
+    JobCategoryId: 20
+  },
+  {
+    id: 2003,
+    guiOrder: 76,
+    chineseDisplayText: "護理",
+    englishDisplayText: "Nursing",
+    JobCategoryId: 20
+  },
+  {
+    id: 2004,
+    guiOrder: 77,
+    chineseDisplayText: "職業治療師",
+    englishDisplayText: "Occupational Therapist",
+    JobCategoryId: 20
+  },
+  {
+    id: 2005,
+    guiOrder: 78,
+    chineseDisplayText: "藥劑",
+    englishDisplayText: "Pharmacy",
+    JobCategoryId: 20
+  },
+  {
+    id: 2006,
+    guiOrder: 79,
+    chineseDisplayText: "心理諮詢",
+    englishDisplayText: "Psychological Counselling",
+    JobCategoryId: 20
+  },
+  {
+    id: 2007,
+    guiOrder: 80,
+    chineseDisplayText: "物理治療師",
+    englishDisplayText: "Physical Therapist",
+    JobCategoryId: 20
+  },
+  {
+    id: 2008,
+    guiOrder: 81,
+    chineseDisplayText: "語言治療師",
+    englishDisplayText: "Speech Therapist",
+    JobCategoryId: 20
+  },
+  {
+    id: 2009,
+    guiOrder: 82,
+    chineseDisplayText: "治療師",
+    englishDisplayText: "Therapist",
+    JobCategoryId: 20
+  },
+  {
+    id: 2099,
+    guiOrder: 83,
+    chineseDisplayText: "醫療服務（其他）",
+    englishDisplayText: "Medical Services (Other)",
+    JobCategoryId: 20
+  },
+  {
+    id: 2100,
+    guiOrder: 84,
+    chineseDisplayText: "農業",
+    englishDisplayText: "Agriculture",
+    JobCategoryId: 21
+  },
+  {
+    id: 2101,
+    guiOrder: 85,
+    chineseDisplayText: "漁業",
+    englishDisplayText: "Fishery",
+    JobCategoryId: 21
+  },
+  {
+    id: 2199,
+    guiOrder: 86,
+    chineseDisplayText: "初級產業（其他）",
+    englishDisplayText: "Primary Industry (Other)",
+    JobCategoryId: 21
+  },
+  {
+    id: 2200,
+    guiOrder: 87,
+    chineseDisplayText: "房地產",
+    englishDisplayText: "Real Estate",
+    JobCategoryId: 22
+  },
+  {
+    id: 2201,
+    guiOrder: 88,
+    chineseDisplayText: "物業管理",
+    englishDisplayText: "Property Management",
+    JobCategoryId: 22
+  },
+  {
+    id: 2299,
+    guiOrder: 89,
+    chineseDisplayText: "物業（其他）",
+    englishDisplayText: "Property (Other)",
+    JobCategoryId: 22
+  },
+  {
+    id: 2300,
+    guiOrder: 90,
+    chineseDisplayText: "銷售",
+    englishDisplayText: "Merchandising",
+    JobCategoryId: 23
+  },
+  {
+    id: 2301,
+    guiOrder: 91,
+    chineseDisplayText: "採購",
+    englishDisplayText: "Purchasing",
+    JobCategoryId: 23
+  },
+  {
+    id: 2399,
+    guiOrder: 92,
+    chineseDisplayText: "零售（其他）",
+    englishDisplayText: "Retail (Other)",
+    JobCategoryId: 23
+  },
+  {
+    id: 2400,
+    guiOrder: 93,
+    chineseDisplayText: "能源",
+    englishDisplayText: "Energy",
+    JobCategoryId: 24
+  },
+  {
+    id: 2401,
+    guiOrder: 94,
+    chineseDisplayText: "食物科學",
+    englishDisplayText: "Food Science",
+    JobCategoryId: 24
+  },
+  {
+    id: 2402,
+    guiOrder: 95,
+    chineseDisplayText: "實驗室",
+    englishDisplayText: "Laboratory",
+    JobCategoryId: 24
+  },
+  {
+    id: 2403,
+    guiOrder: 96,
+    chineseDisplayText: "研究與開發",
+    englishDisplayText: "Research and development",
+    JobCategoryId: 24
+  },
+  {
+    id: 2499,
+    guiOrder: 97,
+    chineseDisplayText: "科學（其他）",
+    englishDisplayText: "Science (Other)",
+    JobCategoryId: 24
+  },
+  {
+    id: 2500,
+    guiOrder: 98,
+    chineseDisplayText: "社會工作者",
+    englishDisplayText: "Social Worker",
+    JobCategoryId: 25
+  },
+  {
+    id: 2599,
+    guiOrder: 99,
+    chineseDisplayText: "社會服務（其他）",
+    englishDisplayText: "Social Service (Other)",
+    JobCategoryId: 25
+  },
+  {
+    id: 2600,
+    guiOrder: 100,
+    chineseDisplayText: "紡織品",
+    englishDisplayText: "Textiles",
+    JobCategoryId: 26
+  },
+  {
+    id: 2700,
+    guiOrder: 101,
+    chineseDisplayText: "AI和機器學習工程師",
+    englishDisplayText: "Ai And Machine Learning Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2701,
+    guiOrder: 102,
+    chineseDisplayText: "後端軟件工程師",
+    englishDisplayText: "Backend Software Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2702,
+    guiOrder: 103,
+    chineseDisplayText: "網絡安全",
+    englishDisplayText: "Cyber Security",
+    JobCategoryId: 27
+  },
+  {
+    id: 2703,
+    guiOrder: 104,
+    chineseDisplayText: "前端軟件工程師",
+    englishDisplayText: "Frontend Software Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2704,
+    guiOrder: 105,
+    chineseDisplayText: "DevOps",
+    englishDisplayText: "DevOps",
+    JobCategoryId: 27
+  },
+  {
+    id: 2705,
+    guiOrder: 106,
+    chineseDisplayText: "數據分析師",
+    englishDisplayText: "Data Analyst",
+    JobCategoryId: 27
+  },
+  {
+    id: 2706,
+    guiOrder: 107,
+    chineseDisplayText: "數據科學家",
+    englishDisplayText: "Data Scientist",
+    JobCategoryId: 27
+  },
+  {
+    id: 2707,
+    guiOrder: 108,
+    chineseDisplayText: "遊戲開發",
+    englishDisplayText: "Game Developer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2708,
+    guiOrder: 109,
+    chineseDisplayText: "手機軟件工程師",
+    englishDisplayText: "Mobile Software Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2709,
+    guiOrder: 110,
+    chineseDisplayText: "系統工程師",
+    englishDisplayText: "Infrastructure Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2710,
+    guiOrder: 111,
+    chineseDisplayText: "物聯網工程師",
+    englishDisplayText: "Iot Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2711,
+    guiOrder: 112,
+    chineseDisplayText: "QA工程師",
+    englishDisplayText: "Qa Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2712,
+    guiOrder: 113,
+    chineseDisplayText: "IT Support",
+    englishDisplayText: "It Support",
+    JobCategoryId: 27
+  },
+  {
+    id: 2713,
+    guiOrder: 114,
+    chineseDisplayText: "機器人工程師",
+    englishDisplayText: "Robotic Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2714,
+    guiOrder: 115,
+    chineseDisplayText: "軟件工程師",
+    englishDisplayText: "Software Engineer",
+    JobCategoryId: 27
+  },
+  {
+    id: 2799,
+    guiOrder: 116,
+    chineseDisplayText: "科技 (其他)",
+    englishDisplayText: "Technology (Other)",
+    JobCategoryId: 27
+  },
+  {
+    id: 2800,
+    guiOrder: 117,
+    chineseDisplayText: "旅遊",
+    englishDisplayText: "Travel",
+    JobCategoryId: 28
+  },
+  {
+    id: 2801,
+    guiOrder: 118,
+    chineseDisplayText: "酒店服務",
+    englishDisplayText: "Hotel Service",
+    JobCategoryId: 28
+  },
+  {
+    id: 2899,
+    guiOrder: 119,
+    chineseDisplayText: "旅遊業（其他）",
+    englishDisplayText: "Tourism (Other)",
+    JobCategoryId: 28
+  },
+  {
+    id: 2900,
+    guiOrder: 120,
+    chineseDisplayText: "翻譯",
+    englishDisplayText: "Translation",
+    JobCategoryId: 29
+  },
+  {
+    id: 3000,
+    guiOrder: 121,
+    chineseDisplayText: "自由工作者",
+    englishDisplayText: "Freelancer",
+    JobCategoryId: 30
+  },
+  {
+    id: 3001,
+    guiOrder: 122,
+    chineseDisplayText: "Slasher",
+    englishDisplayText: "Slasher",
+    JobCategoryId: 30
+  },
+  {
+    id: 3002,
+    guiOrder: 123,
+    chineseDisplayText: "公務員",
+    englishDisplayText: "Civil Servant",
+    JobCategoryId: 30
+  },
+  {
+    id: 3003,
+    guiOrder: 124,
+    chineseDisplayText: "KOL",
+    englishDisplayText: "KOL",
+    JobCategoryId: 30
+  },
+  {
+    id: 3100,
+    guiOrder: 125,
+    chineseDisplayText: "其他",
+    englishDisplayText: "Other",
+    JobCategoryId: 31
+  }
+]
 export {
   // dressCodeOptions,
   // jobSourceOptions,
@@ -475,5 +1359,6 @@ export {
   internshipOfferSortingTypesList,
   gradJobOfferSortingTypesList,
   SITENAME,
-  currentSchoolTypesList
+  currentSchoolTypesList,
+  jobTypeList
 }
