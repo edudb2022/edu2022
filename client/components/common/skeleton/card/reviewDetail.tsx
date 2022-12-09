845eca0552b46513d383dfdcbb03424cef40869b
Pjaijai <paul6a24@hotmail.com>
2022-12-09 22:38:28 +0800

    review detail

--- client/components/common/skeleton/card/reviewDetail.tsx
+++ client/components/common/skeleton/card/reviewDetail.tsx
@@ -1,24 +1,11 @@
 import { Skeleton } from "@mui/material"
 import React from "react"
-import BaseCard from "../../cards"
 
 const ReviewDetailSkeletonCard: React.FunctionComponent = () => {
   return (
-    <BaseCard className={`w-full flex flex-row justify-around  border-black  `}>
-      <div className="flex flex-col w-4/5 h-full justify-between">
-        <Skeleton variant="text" animation="wave" />
-        <Skeleton variant="rectangular" animation="wave" className="h-3/5" />
-        <Skeleton variant="text" animation="wave" />
-      </div>
-
-      <div className="flex justify-center items-center w-1/5">
-        <Skeleton
-          variant="circular"
-          animation="wave"
-          className="w-12 h-12 md:w-24 md:h-24"
-        />
-      </div>
-    </BaseCard>
+    <div className="flex flex-col">
+      <Skeleton variant="rectangular" animation="wave" height={150} />
+    </div>
   )
 }
 
