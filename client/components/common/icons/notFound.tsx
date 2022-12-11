a4cd11a8f42381eefe1a0024d82bbdde344b950b
Pjaijai <paul6a24@hotmail.com>
2022-12-11 21:28:05 +0800

    404 page

---
+++ client/components/common/icons/notFound.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { TbError404 } from "react-icons/tb"
+const NotFound404Icon: React.FunctionComponent = ({ ...props }) => {
+  return <TbError404 {...props} />
+}
+
+export default NotFound404Icon
--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react"
+import ErrorIcon from "../components/common/icons/error"
+import PageLayout from "../components/layouts/page"
+import { TbError404 } from "react-icons/tb"
+import NotFound404Icon from "../components/common/icons/notFound"
 
 const ErrorPage: React.FunctionComponent = () => {
-  return <div>404</div>
+  return (
+    <PageLayout>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+        {/* <div className="flex justify-center items-center border-2 border-red-500 "> */}
+        <div className="flex flex-col justify-center bg-theme-one-50  text-theme-one-500 w-full p-4 rounded-2xl">
+          <div className="text-[10rem] flex flex-row">
+            <ErrorIcon />
+            <NotFound404Icon />
+          </div>
+
+          {/* <div className="text-2xl text-center"> 404 Not found</div> */}
+        </div>
+      </div>
+    </PageLayout>
+  )
 }
 
 export default ErrorPage
