12500c64dc92a53dee703b2661e182de0815fd91
Pjaijai <paul6a24@hotmail.com>
2022-12-11 21:32:21 +0800

    Update 404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -8,15 +8,15 @@ const ErrorPage: React.FunctionComponent = () => {
   return (
     <PageLayout>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-        {/* <div className="flex justify-center items-center border-2 border-red-500 "> */}
         <div className="flex flex-col justify-center bg-theme-one-50  text-theme-one-500 w-full p-4 rounded-2xl">
           <div className="text-[10rem] flex flex-row">
             <ErrorIcon />
             <NotFound404Icon />
           </div>
-
-          {/* <div className="text-2xl text-center"> 404 Not found</div> */}
         </div>
+        <p className="text-end font-mono text-xs text-theme-one-500">
+          By R1R69
+        </p>
       </div>
     </PageLayout>
   )
