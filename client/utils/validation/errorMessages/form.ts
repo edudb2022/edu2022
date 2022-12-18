6c5054213fa539a168479fbe733593f7640682fc
Pjaijai <paul6a24@hotmail.com>
2022-12-18 21:10:34 +0800

    validation error message

--- client/utils/validation/errorMessages/form.ts
+++ client/utils/validation/errorMessages/form.ts
@@ -5,5 +5,6 @@ export enum ERROR_FORM_MESSAGES {
   TOO_LONG = "太長啦！",
   SALARY_NEGATIVE = "負數人工?",
   GPA_NEGATIVE = "負數GPA? 你點攞的？",
-  GPA_TOO_LARGE = "GPA>4.3? 你點攞的?"
+  GPA_TOO_LARGE = "GPA>4.3? 你點攞的?",
+  NOT_SUBJECT_DUPLICATION = "好似重覆咗?"
 }
