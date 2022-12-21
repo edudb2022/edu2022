f73d014009233262c82ef3b35c68a6294fcf6921
Pjaijai <paul6a24@hotmail.com>
2022-12-21 12:06:44 +0800

    update form validation schema

--- client/utils/validation/form/schema.ts
+++ client/utils/validation/form/schema.ts
@@ -1,5 +1,9 @@
 import * as yup from "yup"
+import { ContactMethodTypeId } from "../../../types/common"
 import { ERROR_FORM_MESSAGES } from "../errorMessages/form"
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
 const TitleValidationSchema = yup
   .string()
   .max(25, ERROR_FORM_MESSAGES.TOO_LONG)
@@ -12,6 +16,26 @@ const DateValidationSchema = yup.date().required("必須填寫").nullable(true)
 const longQuestionValidationSchema = yup
   .string()
   .max(3000, ERROR_FORM_MESSAGES.TOO_LONG)
+
+const contactDetailValidationSchema = yup
+  .string()
+  .when("contactMethod", (contactMethod, schema) => {
+    if (contactMethod === ContactMethodTypeId.EMAIL)
+      return yup
+        .string()
+        .email(ERROR_FORM_MESSAGES.NOT_A_VALID_EMAIL)
+        .required(ERROR_FORM_MESSAGES.REQUIRED)
+    else if (
+      contactMethod === ContactMethodTypeId.SIGNAL ||
+      contactMethod === ContactMethodTypeId.WHATSAPP
+    )
+      return yup
+        .string()
+        .matches(phoneRegExp, ERROR_FORM_MESSAGES.NOT_A_VALID_PHONE_NUMBER)
+        .required(ERROR_FORM_MESSAGES.REQUIRED)
+    else return yup.string()
+  })
+  .nullable(true)
 
 const SelectCommonValidationSchema = yup
   .string()
@@ -28,5 +52,6 @@ export {
   SelectCommonValidationSchema,
   SalaryValidationSchema,
   DateValidationSchema,
-  longQuestionValidationSchema
+  longQuestionValidationSchema,
+  contactDetailValidationSchema
 }
